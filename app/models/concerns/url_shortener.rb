da8e8305bbf83ae44433c4eb5329c90721d7883c
Garrett Boone <garrettboone@dbc-garrettboone.local>
2018-03-14 16:26:52 -0500

    change variable names

--- app/models/concerns/url_shortener.rb
+++ app/models/concerns/url_shortener.rb
@@ -1,19 +1,19 @@
 require 'google/apis/urlshortener_v1'
 module UrlShortener
 
-  def generate_short_url(url)
+  def generate_short_url(our_url)
     url = Google::Apis::UrlshortenerV1::Url.new
-    url.long_url = url
-    insert_url(url).id
+    url.long_url = our_url
+    create_url(url).id
   rescue => e
     puts "Oh Noes! :( Google Errored"
     # log an error of some sort. Notify the user and someone that can debug.
   end
 
 
-  def insert_url(url)
+  def create_url(google_url)
     google_shortener_service = Google::Apis::UrlshortenerV1::UrlshortenerService.new
     google_shortener_service.key = Rails.application.secrets.google_api_key
-    google_shortener_service.insert_url(url)
+    google_shortener_service.insert_url(google_url)
   end
 end
