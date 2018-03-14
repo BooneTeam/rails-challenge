c85f95918f3541356c2fd6a4d0762c6b9bee87d6
Garrett Boone <garrettboone@dbc-garrettboone.local>
2018-03-14 16:26:17 -0500

    simple nokogiri scrape

--- app/models/concerns/content_pullable.rb
+++ app/models/concerns/content_pullable.rb
@@ -1,5 +1,17 @@
 module ContentPullable
 
+  def pull_content(url)
+    page = get_website(url)
+    parse_header_tags(page)
+  end
 
+  def get_website(url)
+    Nokogiri::HTML(open(url))
+  end
 
+  def parse_header_tags(page)
+    ['h1','h2','h3'].map! do |tag|
+      page.css(tag).map{|t| t.text.strip }
+    end.flatten
+  end
 end
