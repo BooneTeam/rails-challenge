b2038337684dba818f2bd5d1e182fefa8c9733ef
Garrett Boone <garrettboone@dbc-garrettboone.local>
2018-03-14 16:25:58 -0500

    add more user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     # should paginate
     @new_user = User.new
@@ -18,6 +19,28 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @friends = @user.all_friends
+    # Should Paginate
+    @users = User.all
+  end
+
+  def tag_search
+    #  need this for form.
+    # Probably wouldn't use same display, probably use Javascript instead of just refreshing
+    @new_user = User.new
+    @users = User.all
+    if params[:friend_scoped ] && params[:q]
+      # Gross
+      @user = User.find_by(id: params[:user_id])
+      @friends = @user.all_friends
+      matched_users = User.tagged_with(params[:q], wild: true, any: true)
+      @matched_users = matched_users - @friends
+      # change this to a different template
+      render :show
+    elsif params[:q]
+      @matched_users = User.tagged_with(params[:q], wild: true, any: true)
+      render :index
+    end
   end
 
   private
